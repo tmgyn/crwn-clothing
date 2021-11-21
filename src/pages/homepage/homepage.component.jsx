ae79eed17c1945e38aa02fbefe6cd3c739906a24
tmgyn <tomn28656@gmail.com>
2021-11-21 13:16:29 -0500

    update

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from "../../components/directory/directory.component";
+import "./homepage.style.scss";
+
+const HomePage = () => (
+  <div className="homePage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
+
+
